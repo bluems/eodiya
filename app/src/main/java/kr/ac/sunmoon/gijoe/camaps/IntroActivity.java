5cbf3a8dc3d1c7d7d939d719f4224e73e0574cb3
BlueMS <bluems@bluems.re.kr>
2020-04-20 05:52:47 +0900

    인트로 화면 추가

--- app/src/main/java/kr/ac/sunmoon/gijoe/camaps/IntroActivity.java
+++ app/src/main/java/kr/ac/sunmoon/gijoe/camaps/IntroActivity.java
@@ -3,6 +3,9 @@ package kr.ac.sunmoon.gijoe.camaps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.content.Intent;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        IntroThread introThread = new IntroThread(handler);
+        introThread.start();
     }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 1) {
+                Intent intent = new Intent(IntroActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        }
+    };
 }
