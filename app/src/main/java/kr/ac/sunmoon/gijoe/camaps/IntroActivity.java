6af10654e8fda3f2460f10f740253e3157499ea7
bluems <bluems@bluems.re.kr>
2020-04-20 05:31:47 +0900

    Revert "인트로 화면 추가"
    This reverts commit c3469935831f1d4cb3fefc71ba725cf9888c2c71.

--- app/src/main/java/kr/ac/sunmoon/gijoe/camaps/IntroActivity.java
+++ app/src/main/java/kr/ac/sunmoon/gijoe/camaps/IntroActivity.java
@@ -2,10 +2,7 @@ package kr.ac.sunmoon.gijoe.camaps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -13,18 +10,5 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-
-        IntroThread introThread = new IntroThread(handler);
-        introThread.start();
     }
-
-    Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            if(msg.what == 1) {
-                Intent intent = new Intent(IntroActivity.this, MapsActivity.class);
-                startActivity(intent);
-            }
-        }
-    };
 }
