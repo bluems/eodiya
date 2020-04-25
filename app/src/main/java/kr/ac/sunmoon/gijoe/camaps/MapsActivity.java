4d02785978d600c0d088e25c6f0041ac1572c357
BlueMS <bluems@bluems.re.kr>
2020-04-26 02:41:41 +0900

    전국공공시설 공공데이터를 XML 파싱하기 위한 코드 추가

--- app/src/main/java/kr/ac/sunmoon/gijoe/camaps/MapsActivity.java
+++ app/src/main/java/kr/ac/sunmoon/gijoe/camaps/MapsActivity.java
@@ -1,6 +1,7 @@
 package kr.ac.sunmoon.gijoe.camaps;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RawRes;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
@@ -28,7 +29,15 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.w3c.dom.Text;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -46,10 +55,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
 
         if (!checkLocationServicesStatus()) {
-
             showDialogForLocationServiceSetting();
         }else {
-
             checkRunTimePermission();
         }
 
@@ -59,6 +66,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         Objects.requireNonNull(mapFragment).getMapAsync(this);
+
+        ArrayList<PublicData> arrayList = xml_parse(R.raw.cheonan);
+        Log.d("MapsActivity", "onCreate: Array Size: " + arrayList.size());
     }
 
     @Override
@@ -75,15 +85,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onRequestPermissionsResult(int permsRequestCode, @NonNull String[] permissions, @NonNull int[] grandResults) {
+        // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
         if ( permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
-
-            // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
-
             boolean check_result = true;
 
-
-            // 모든 퍼미션을 허용했는지 체크합니다.
-
+            // 모든 퍼미션을 허용했는지 체크
             for (int result : grandResults) {
                 if (result != PackageManager.PERMISSION_GRANTED) {
                     check_result = false;
@@ -91,57 +97,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             }
 
-
             if ( !check_result ) {
-                // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있습니다.
-
+                // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료
                 if (shouldShowRequestPermissionRationale(REQUIRED_PERMISSIONS[0])
                         || shouldShowRequestPermissionRationale(REQUIRED_PERMISSIONS[1])) {
-
                     Toast.makeText(MapsActivity.this, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.", Toast.LENGTH_LONG).show();
                     finish();
-
-
                 }else {
-
                     Toast.makeText(MapsActivity.this, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ", Toast.LENGTH_LONG).show();
-
                 }
             }
-
         }
     }
-
-    private void showDialogForLocationServiceSetting() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
-        builder.setTitle("위치 서비스 비활성화");
-        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
-                + "위치 설정을 수정하실래요?");
-        builder.setCancelable(true);
-        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                Intent callGPSSettingIntent
-                        = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
-            }
-        });
-        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                dialog.cancel();
-            }
-        });
-        builder.create().show();
-    }
-
-    private boolean checkLocationServicesStatus() {
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        return Objects.requireNonNull(locationManager).isProviderEnabled(LocationManager.GPS_PROVIDER)
-                || Objects.requireNonNull(locationManager).isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-    }
-
 
     /**
      * Manipulates the map once available.
@@ -182,6 +149,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.getUiSettings().setMyLocationButtonEnabled(true);
     }
 
+    //권한 설정 연결
+    private void showDialogForLocationServiceSetting() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
+        builder.setTitle("위치 서비스 비활성화");
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.");
+        builder.setCancelable(true);
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Intent callGPSSettingIntent
+                        = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
+    }
+
+    //서비스 사용 가능 여부 판단
+    private boolean checkLocationServicesStatus() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        return Objects.requireNonNull(locationManager).isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || Objects.requireNonNull(locationManager).isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    //서비스 권한 사용 가능 여부 판단
     void checkRunTimePermission() {
         int hasFineLocationPermission = ContextCompat.checkSelfPermission(MapsActivity.this,
                 Manifest.permission.ACCESS_FINE_LOCATION);
@@ -198,5 +197,113 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 requestPermissions(REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE);
             }
         }
+    }
+
+    private ArrayList<PublicData> xml_parse(@RawRes int id) {
+        String TAG = "Parser";
+        ArrayList<PublicData> publicDataList = new ArrayList<>();
+
+        InputStream inputStream = getResources().openRawResource(id);
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+
+        XmlPullParserFactory xmlPullParserFactory;
+        XmlPullParser xmlPullParser;
+
+        try {
+            xmlPullParserFactory = XmlPullParserFactory.newInstance();
+            xmlPullParser = xmlPullParserFactory.newPullParser();
+            xmlPullParser.setInput(reader);
+
+            PublicData publicData = null;
+            int eventType = xmlPullParser.getEventType();
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
+                    case XmlPullParser.START_DOCUMENT:
+                        Log.d(TAG,"xml_parse: Start");
+                        break;
+                    case XmlPullParser.START_TAG:
+                        String startTag = xmlPullParser.getName();
+                        Log.d(TAG, "xml_parse: Start TAG: " + xmlPullParser.getName());
+
+                        if(startTag.equals("Row")) {
+                            publicData = new PublicData();
+                            Log.d(TAG, "xml_parse: 공공데이터 Row 추가");
+                        }
+
+                        if (publicData != null)
+                            if (startTag.equals("개방시설명")) {
+                                publicData.setFacility(xmlPullParser.nextText());
+                            }
+                            else if (startTag.equals("개방장소명")) {
+                                publicData.setPlace(xmlPullParser.nextText());
+                            }
+                            else if (startTag.equals("휴관일")) {
+                                publicData.setClosed(xmlPullParser.nextText());
+                            }
+                            else if (startTag.equals("평일운영시작시각")) {
+                                publicData.setWeekday(xmlPullParser.nextText() + "~" + publicData.getWeekday());
+                            }
+                            else if (startTag.equals("평일운영종료시각")) {
+                                publicData.setWeekday(publicData.getWeekday() + xmlPullParser.nextText());
+                            }
+                            else if (startTag.equals("주말운영시작시각")) {
+                                publicData.setWeekend(xmlPullParser.nextText() + "~" + publicData.getWeekend());
+                            }
+                            else if (startTag.equals("주말운영종료시각")) {
+                                publicData.setWeekend(publicData.getWeekend() + xmlPullParser.nextText());
+                            }
+                            else if (startTag.equals("유료사용여부")) {
+                                publicData.setPaid(xmlPullParser.nextText());
+                            }
+                            else if (startTag.equals("부대시설정보")) {
+                                publicData.setDesc(xmlPullParser.nextText());
+                            }
+                            else if (startTag.equals("신청방법구분")) {
+                                publicData.setApply(xmlPullParser.nextText());
+                            }
+                            else if (startTag.equals("소재지도로명주소")) {
+                                publicData.setAddress(xmlPullParser.nextText());
+                            }
+                            else if (startTag.equals("사용안내전화번호")) {
+                                publicData.setTel(xmlPullParser.nextText());
+                            }
+                            else if (startTag.equals("홈페이지주소")) {
+                                publicData.setUrl(xmlPullParser.nextText());
+                            }
+                            else if (startTag.equals("위도")) {
+                                publicData.setLatitude(Double.parseDouble(xmlPullParser.nextText()));
+                            }
+                            else if (startTag.equals("경도")) {
+                                publicData.setLongitude(Double.parseDouble(xmlPullParser.nextText()));
+                            }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        String endTag = xmlPullParser.getName();
+                        Log.d(TAG, "xml_parse: End TAG: " + endTag);
+
+                        if(endTag.equals("Row")) {
+                            publicDataList.add(publicData);
+                        }
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+
+            Log.d(TAG, "xml_parse: End");
+        } catch (XmlPullParserException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+                inputStreamReader.close();
+                inputStream.close();
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+
+        return publicDataList;
     }
 }
