5fd16eaa1a7e0b9c3ed03da8bd415532cd3bb0e6
BlueMS <bluems@bluems.re.kr>
2020-06-09 16:17:14 +0900

    동작 안되는 Retrofit2 제거

--- app/src/main/java/kr/ac/sunmoon/gijoe/camaps/signupActivity.java
+++ app/src/main/java/kr/ac/sunmoon/gijoe/camaps/signupActivity.java
@@ -4,21 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import java.io.IOException;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
 public class signupActivity extends AppCompatActivity {
-    private Retrofit mRetrofit;
-    private authApi mAuthAPI;
-    private Call<SignupAPI> mCallSignUp;
 
     Button signupBtn;
     EditText signupID;
@@ -30,36 +22,27 @@ public class signupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        setRetrofitInit();
+        initializeView();
     }
 
     protected void initializeView() {
         signupBtn = findViewById(R.id.callSignupBtn);
+        signupID = findViewById(R.id.field_signup_id);
+        signupPW = findViewById(R.id.field_signup_password);
+        signupNick = findViewById(R.id.field_signup_nickname);
+    }
+
+    protected void SetListener() {
+        signupBtn.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                signup(signupID.getText().toString(), signupPW.getText().toString(), signupNick.getText().toString());
+            }
+        });
     }
 
     private void signup(String username, String password, String nickname) {
-        mCallSignUp = mAuthAPI.signUp(username,password,nickname);
-        mCallSignUp.enqueue(mRetrofitCallback);
+
     }
-
-    private void setRetrofitInit() {
-        mRetrofit = new Retrofit.Builder()
-                .baseUrl(getString(R.string.baseUrl))
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        mAuthAPI = mRetrofit.create(authApi.class);
-    }
-
-    private Callback<SignupAPI> mRetrofitCallback = new Callback<SignupAPI>() {
-        @Override
-        public void onResponse(Call<SignupAPI> call, Response<SignupAPI> response) {
-            Log.v("Test", response.body().toString());
-        }
-
-        @Override
-        public void onFailure(Call<SignupAPI> call, Throwable t) {
-            t.printStackTrace();
-        }
-    };
 }
