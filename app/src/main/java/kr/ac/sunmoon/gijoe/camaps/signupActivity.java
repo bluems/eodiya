afea9f2978765aef260728867c1a3c5f4e83e157
BlueMS <bluems@bluems.re.kr>
2020-06-15 00:33:20 +0900

    테스트하던 post통신 서비스 삭제

--- app/src/main/java/kr/ac/sunmoon/gijoe/camaps/signupActivity.java
+++ app/src/main/java/kr/ac/sunmoon/gijoe/camaps/signupActivity.java
@@ -2,20 +2,31 @@ package kr.ac.sunmoon.gijoe.camaps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class signupActivity extends AppCompatActivity {
 
-    Button signupBtn;
-    EditText signupID;
-    EditText signupPW;
-    EditText signupNick;
+    private Button signupBtn;
+    private EditText signupID;
+    private EditText signupPW;
+    private EditText signupNick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,7 @@ public class signupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         initializeView();
+        SetListener();
     }
 
     protected void initializeView() {
